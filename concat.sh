e287ef4b72cdfaef5a33b1a67a498d799f08a092
Ali Shah <ali@titus.local>
2018-06-03 15:22:16 -0400

    new version of concat script

--- concat.sh
+++ concat.sh
@@ -1,45 +1,60 @@
 #!/bin/bash
-IN_PATH=$1
-OUTPUT=$2
+# Depends now on coreutils (for realpath):
+#       $ brew install coreutils
+
+INPUT_PATH=$1
+OUTPUT_PATH=$2
+OUTPUT_FN=$3
+
+IN_PATH=$(realpath $INPUT_PATH 2>/dev/null)
 if [[ $IN_PATH == "" ]]; then
-    echo "Missing input path parameter"
-    echo "Usage $0 <input path> <output file>"
+    echo "Missing input path parameter: $INPUT_PATH is not valid"
+    echo "Usage $0 <input path> <output path> <(optional) output file name>"
     exit
 fi
-IN_PATH=$PWD/$IN_PATH
 
-if [[ $OUTPUT == "" ]]; then
-    echo "Missing output file parameter.. taking a guess"
-    OUTPUT=$(ls $IN_PATH | sort | head -1)
-    OUTPUT=$(basename $OUTPUT .mp4)_full.mp4
-    echo "Determined output to be: $OUTPUT"
+OUT_PATH=$(realpath $OUTPUT_PATH)
+if [[ $OUT_PATH == "" ]]; then
+    echo "Missing output path parameter: $OUTPUT_PATH is not valid"
+    echo "Usage $0 <input path> <output path> <(optional) output file name>"
+    exit
 fi
 
-if [[ $OUTPUT == "" ]]; then
+if [[ ! -d $OUT_PATH ]]; then
+    echo "Output directory is invalid: $OUT_PATH is not a directory"
+    exit
+fi
+
+
+if [[ $OUTPUT_FN == "" ]]; then
+    echo "Missing output file parameter.. taking a guess"
+    OUTPUT_FN=$(ls $IN_PATH | sort | head -1)
+    OUTPUT_FN=$(basename $OUTPUT_FN .mp4)_full.mp4
+    echo "Determined output to be: $OUTPUT_FN"
+fi
+
+if [[ $OUTPUT_FN == "" ]]; then
     echo "Guessed output file invalid, exiting"
     exit
 fi
 
-OUTPUT_DIR=$(dirname $OUTPUT)
-if [[ ! -d $OUTPUT_DIR ]]; then
-    echo "Output location $OUTPUT_DIR is not directory"
-    exit
-fi
+OUTPUT_FILE_PATH=$OUT_PATH/$OUTPUT_FN
+echo "OUTPUT: $OUTPUT_FILE_PATH"
 
 TMP=$(mktemp)
+echo "Tmp file is $TMP:"
 i=0;
 for file in $(ls $IN_PATH | grep mp4 | sort)
 do
+    file=$(realpath $IN_PATH/$file)
     if [[ ! -f $file ]];
     then
         echo "Skipping $file"
         continue;
     fi
-    echo "[$i]: $PWD/$file"
-    echo "file '$PWD/$file'" >> $TMP
+    echo -e "\t [$i]: $file"
+    echo "file '$file'" >> $TMP
     ((i++))
 done
-echo "Tmp file is $TMP"
-echo "OUTPUT: $OUTPUT"
 
-ffmpeg -f concat -safe 0 -i $TMP -c copy $OUTPUT
+ffmpeg -f concat -safe 0 -i $TMP -c copy $OUTPUT_FILE_PATH
